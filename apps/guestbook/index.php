c9bfc8f4df0f99ef0bacb390708e2438a944b071
Jon Saxton <jjonsaxton@gmail.com>
2023-08-05 15:02:03 -0700

    Added CSV processor to guestbook app

--- apps/guestbook/index.php
+++ apps/guestbook/index.php
@@ -1,6 +1,73 @@
 <?php
 // Show and create new guestbook entries!
 
+class FileCSV
+{
+  public $uri;
+  public $raw;
+
+  public function __construct($uri)
+  {
+    $this->uri=$uri;
+    if (is_file($uri) && (is_readable($uri)))
+    {
+      $this->raw=trim(file_get_contents($uri));
+    }
+  }
+
+  public function read()
+  {
+    $lines=explode(PHP_EOL,$this->raw);
+    $cols=str_getcsv($lines[0]);
+    unset ($lines[0]);
+    $data=array();
+    foreach ($lines as $line)
+    {
+      $row=str_getcsv($line);
+      $col_index=0;
+      foreach ($row as $val)
+      {
+        $row_data[$cols[$col_index]]=$val;
+        $col_index++;
+      }
+      unset($col_index);
+      $data[]=$row_data;
+    }
+    return $data;
+  }
+
+  public function write(array $data)
+  {
+    if (is_array($data))
+    {
+      if ($empty($this->content))
+      {
+        $cols=array_keys($data[0]);
+        $csv=implode(",",$cols); //uses column names as first row of CSV data
+        unset($cols);
+        $csv=trim($csv,",")."\n";
+      }
+      else
+      {
+        $csv=$this->content."\n";
+      }
+
+      foreach ($data as $row)
+      {
+        foreach ($row as $val)
+        {
+          $csv.="\"".$val."\",";
+        }
+        $csv=trim($csv,",")."\n";
+      }
+
+      return file_put_contents($this->uri,$csv);
+    }
+  }
+}
+
+$file=new FileCSV("./entries.csv");
+
 if (@$_POST['name']) //assume a new entry needs to be created!
 {
     //TODO add a new guest book entry to the .csv file and redirect back to main page if successful, show an error message if not!
@@ -18,6 +85,30 @@ else
     <p>In the 1990's just about every site that wasn't a commercial site had a guestbook. It was away for people to leave a message and tell people they were on a site. For the most part these were likely hosted by a third-party company since many free hosting services didn't offer any sort of server-side scripting or application support. Many of these are dead now, so I wrote a quick and easy version here to demonstrate what they were like. Of course, the bonus here is that you get to experience a guestbook! Go ahead and sign it and see what happens!</p>
 <?php
 //TODO show current guestbook entries!
+$entries=$file->read();
+foreach ($entries as $entry)
+{
+  if (!empty($entry['Email']) && !empty($entry['Name']))
+  {
+    $author="<a href=\"mailto:{$entry['Email']}\">{$entry['Name']}</a>";
+  }
+  elseif (!empty($entry['Name']))
+  {
+    $author="{$entry['Name']}";
+  }
+  elseif (!empty($entry['Email']))
+  {
+    $author="<a href=\"mailto:{$entry['Email']}\">{$entry['Email']}</a>";
+  }
+  else
+  {
+    $author="Anonymous";
+  }
+
+  $posted=date("Y m d H:i",$entry['Timestamp']);
+
+  print "<div class=\"guestbook entry\" id=\"{$entry['Timestamp']}\">\n<div class=\"info\">{$author} - {$posted}</div>\n<div class=\"message\"><p>{$entry['Message']}</p></div>\n</div>\n";
+}
 //TODO show add guestbook form!
 ?>
   </body>
